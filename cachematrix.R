570847e0383229c4b0e751ea0d6afe85459ca97e
Algebraist314 <b47keating@gmail.com>
2019-10-19 15:06:01 +0100

    version 2: updated comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,14 @@
 ## Define two functions, one to cache a matrix inverse, another to either calculate a matrix inverse or retrieve
 ## a cached value
 
-## Function to cache a matrix inverse, and define a set of useful functions
+## Example usage:
+## z <- makeCacheMatrix()
+## z$set(matrix(c(1,2,-5,6), nrow = 2))
+## cacheSolve(z)
+
+## Function to define a set of functions to update and access cached values
+## `get` and `set` will return and update the cached matrix
+## `getinv` and `setin` will return and update the cached matrix inverse
 
 makeCacheMatrix <- function(x = matrix()) {
     
@@ -20,7 +27,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Function to access cached value of matrix inverse or calculate if not previously cachec
+## Function to access cached value of matrix inverse or calculate if not previously cached, and return the inverse
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
